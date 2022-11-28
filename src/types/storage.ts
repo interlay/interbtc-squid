5f39a8756990417b59e8ee0943782080fde1494b
Peter <peter@interlay.io>
2022-11-28 12:45:28 +0100

    chore: merge new event types with old

--- src/types/storage.ts
+++ src/types/storage.ts
@@ -33,6 +33,25 @@ export class IssueIssuePeriodStorage {
   }
 
   /**
+   *  The time difference in number of blocks between an issue request is created
+   *  and required completion time by a user. The issue period has an upper limit
+   *  to prevent griefing of vault collateral.
+   */
+  get isV1019000() {
+    return this._chain.getStorageItemTypeHash('Issue', 'IssuePeriod') === '81bbbe8e62451cbcc227306706c919527aa2538970bd6d67a9969dd52c257d02'
+  }
+
+  /**
+   *  The time difference in number of blocks between an issue request is created
+   *  and required completion time by a user. The issue period has an upper limit
+   *  to prevent griefing of vault collateral.
+   */
+  async getAsV1019000(): Promise<number> {
+    assert(this.isV1019000)
+    return this._chain.getStorage(this.blockHash, 'Issue', 'IssuePeriod')
+  }
+
+  /**
    * Checks whether the storage item is defined for the current chain version.
    */
   get isExists(): boolean {
@@ -68,6 +87,25 @@ export class RedeemRedeemPeriodStorage {
    */
   async getAsV1(): Promise<number> {
     assert(this.isV1)
+    return this._chain.getStorage(this.blockHash, 'Redeem', 'RedeemPeriod')
+  }
+
+  /**
+   *  The time difference in number of blocks between a redeem request is created and required completion time by a
+   *  vault. The redeem period has an upper limit to ensure the user gets their BTC in time and to potentially
+   *  punish a vault for inactivity or stealing BTC.
+   */
+  get isV1019000() {
+    return this._chain.getStorageItemTypeHash('Redeem', 'RedeemPeriod') === '81bbbe8e62451cbcc227306706c919527aa2538970bd6d67a9969dd52c257d02'
+  }
+
+  /**
+   *  The time difference in number of blocks between a redeem request is created and required completion time by a
+   *  vault. The redeem period has an upper limit to ensure the user gets their BTC in time and to potentially
+   *  punish a vault for inactivity or stealing BTC.
+   */
+  async getAsV1019000(): Promise<number> {
+    assert(this.isV1019000)
     return this._chain.getStorage(this.blockHash, 'Redeem', 'RedeemPeriod')
   }
 
