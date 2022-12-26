4f8710ce35b4b566ae9dfa8f0f05ff3d8fb38d3b
Matthias Funke <matthias.funke@alation.com>
2022-12-26 11:14:15 +0700

    investigating why metadata for loandata not there

--- src/mappings/event/loans.ts
+++ src/mappings/event/loans.ts
@@ -2,7 +2,7 @@ import { SubstrateBlock, toHex } from "@subsquid/substrate-processor";
 import { LessThanOrEqual } from "typeorm";
 import {
     CumulativeVolume,
-    CumulativeVolumePerCurrencyPair,
+    CumulativeVolumePerCurrencyPair, LendToken,
     Redeem,
     RedeemCancellation,
     RedeemExecution,
@@ -10,19 +10,18 @@ import {
     RedeemRequest,
     RedeemStatus,
     RelayedBlock,
-    VolumeType,
+    VolumeType
 } from "../../model";
 import { Ctx, EventItem } from "../../processor";
 import {
     LoansNewMarketEvent,
 } from "../../types/events";
-import {
-    address,
-    currencyId,
-    encodeLegacyVaultId,
-    encodeVaultId,
-    legacyCurrencyId,
-} from "../encoding";
+import { CurrencyId_Token as CurrencyId_Token_V6 } from "../../types/v6";
+import { CurrencyId_Token as CurrencyId_Token_V10 } from "../../types/v10";
+import { CurrencyId_Token as CurrencyId_Token_V15 } from "../../types/v15";
+import { CurrencyId as CurrencyId_V17 } from "../../types/v17";
+import { CurrencyId as CurrencyId_V1020000 } from "../../types/v1020000";
+import { address, currencyId, legacyCurrencyId } from "../encoding";
 import {
     updateCumulativeVolumes,
     updateCumulativeVolumesForCurrencyPair,
@@ -31,6 +30,7 @@ import EntityBuffer from "../utils/entityBuffer";
 import { blockToHeight } from "../utils/heights";
 import { getCurrentRedeemPeriod } from "../utils/requestPeriods";
 import { getVaultId, getVaultIdLegacy } from "../_utils";
+import { LoanMarket } from "../../model/generated/loanMarket.model";
 
 export async function newMarket(
     ctx: Ctx,
@@ -39,41 +39,18 @@ export async function newMarket(
     entityBuffer: EntityBuffer
 ): Promise<void> {
     const rawEvent = new LoansNewMarketEvent(ctx, item.event);
-    let e;
-    let vault;
-    let vaultIdString;
-    // if (rawEvent.isV6 || rawEvent.isV15) {
-    //     // legacy encodings
-    //     if (rawEvent.isV6) e = rawEvent.asV6;
-    //     else e = rawEvent.asV15;
-    //     vault = await getVaultIdLegacy(ctx.store, e.vaultId);
-    //     vaultIdString = encodeLegacyVaultId(e.vaultId);
-    // } else {
-    //     if (rawEvent.isV17) e = rawEvent.asV17;
-    //     else if (rawEvent.isV1019000) e = rawEvent.asV1019000;
-    //     else {
-    //         e = rawEvent.asV1020000;
-    //         if (!rawEvent.isV1020000)
-    //             ctx.log.warn(`UNKOWN EVENT VERSION: Redeem.requestRedeem`);
-    //     }
-    //
-    //     vault = await getVaultId(ctx.store, e.vaultId);
-    //     vaultIdString = encodeVaultId(e.vaultId);
-    // }
-    //
-    // if (vault === undefined) {
-    //     ctx.log.warn(
-    //         `WARNING: no vault ID found for issue request ${toHex(
-    //             e.redeemId
-    //         )}, with encoded account-wrapped-collateral ID of ${vaultIdString} (at parachain absolute height ${
-    //             block.height
-    //         }`
-    //     );
-    //     return;
-    // }
-    //
-    // const period = await getCurrentRedeemPeriod(ctx, block);
-    //
+    let [currency_id, market] = rawEvent.asV1020000;
+    const currency = new LendToken()
+    currency.lendTokenId = currency_id.value as number
+    const height = await blockToHeight(ctx, block.height, "NewMarket");
+    const timestamp = new Date(block.timestamp);
+    const my_market = new LoanMarket({
+        id: `Market created at ${timestamp}`,
+        token: currency,
+        height: height,
+        timestamp: timestamp
+    });
+    console.log(JSON.stringify(my_market));
     // const redeem = new Redeem({
     //     id: toHex(e.redeemId),
     //     bridgeFee: e.fee,
@@ -110,6 +87,6 @@ export async function newMarket(
     //     backingHeight: backingBlock?.backingHeight || 0,
     // });
     //
-    // await entityBuffer.pushEntity(Redeem.name, redeem);
+    await entityBuffer.pushEntity(LoanMarket.name, my_market);
 }
 
