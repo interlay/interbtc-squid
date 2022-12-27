26983e48f54c37faeb643594ce2bf7b3b8129853
Matthias Funke <matthias.funke@alation.com>
2022-12-27 19:31:50 +0700

    Added all events and tested catching borrow

--- src/mappings/event/loans.ts
+++ src/mappings/event/loans.ts
@@ -9,12 +9,17 @@ import {
     RedeemPeriod,
     RedeemRequest,
     RedeemStatus,
-    RelayedBlock,
+    RelayedBlock, Transfer,
     VolumeType
 } from "../../model";
 import { Ctx, EventItem } from "../../processor";
 import {
-    LoansNewMarketEvent,
+    LoansBorrowedEvent,
+    LoansDepositCollateralEvent,
+    LoansDepositedEvent,
+    LoansDistributedBorrowerRewardEvent,
+    LoansDistributedSupplierRewardEvent,
+    LoansNewMarketEvent, LoansRepaidBorrowEvent
 } from "../../types/events";
 import { CurrencyId_Token as CurrencyId_Token_V6 } from "../../types/v6";
 import { CurrencyId_Token as CurrencyId_Token_V10 } from "../../types/v10";
@@ -55,3 +60,71 @@ export async function newMarket(
     await entityBuffer.pushEntity(LoanMarket.name, my_market);
 }
 
+export async function borrow(
+    ctx: Ctx,
+    block: SubstrateBlock,
+    item: EventItem,
+    entityBuffer: EntityBuffer
+): Promise<void> {
+    const rawEvent = new LoansBorrowedEvent(ctx, item.event);
+    const [adress, currencyOfLoan, amount] = rawEvent.asV1020000;
+    const currency = currencyId.encode(currencyOfLoan);
+    const height = await blockToHeight(ctx, block.height, "LoansBorrowed");
+    const account = address.interlay.encode(adress);
+    // await entityBuffer.pushEntity(
+    //     Transfer.name,
+    //     new Transfer({
+    //         id: item.event.id,
+    //         height,
+    //         timestamp: new Date(block.timestamp),
+    //         account: account,
+    //         token: currency,
+    //         amount,
+    //     })
+    console.log(`${adress} borrowed ${amount} ${currency}`);
+}
+
+export async function depositCollateral(
+    ctx: Ctx,
+    block: SubstrateBlock,
+    item: EventItem,
+    entityBuffer: EntityBuffer
+): Promise<void> {
+    const rawEvent = new LoansDepositCollateralEvent(ctx, item.event);
+}
+
+export async function depositForLending(
+    ctx: Ctx,
+    block: SubstrateBlock,
+    item: EventItem,
+    entityBuffer: EntityBuffer
+): Promise<void> {
+    const rawEvent = new LoansDepositedEvent(ctx, item.event);
+}
+
+export async function distributeBorrowerReward(
+    ctx: Ctx,
+    block: SubstrateBlock,
+    item: EventItem,
+    entityBuffer: EntityBuffer
+): Promise<void> {
+    const rawEvent = new LoansDistributedBorrowerRewardEvent(ctx, item.event);
+}
+
+export async function distributeSupplierReward(
+    ctx: Ctx,
+    block: SubstrateBlock,
+    item: EventItem,
+    entityBuffer: EntityBuffer
+): Promise<void> {
+    const rawEvent = new LoansDistributedSupplierRewardEvent(ctx, item.event);
+}
+
+export async function repay(
+    ctx: Ctx,
+    block: SubstrateBlock,
+    item: EventItem,
+    entityBuffer: EntityBuffer
+): Promise<void> {
+    const rawEvent = new LoansRepaidBorrowEvent(ctx, item.event);
+}
