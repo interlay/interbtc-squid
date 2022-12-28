2adbb4d6c3a9bd751196c552e99ea94ba23bb0c8
Caron S <cschaller@blockdaemon.com>
2022-12-28 10:41:35 +0100

    rateModel fixes

--- src/model/generated/_rateModel.ts
+++ src/model/generated/_rateModel.ts
@@ -4,9 +4,9 @@ import {RateModelCurve} from "./_rateModelCurve"
 export type RateModel = RateModelJump | RateModelCurve
 
 export function fromJsonRateModel(json: any): RateModel {
-  switch(json?.isTypeOf) {
-    case 'RateModelJump': return new RateModelJump(undefined, json)
-    case 'RateModelCurve': return new RateModelCurve(undefined, json)
-    default: throw new TypeError('Unknown json object passed as RateModel')
-  }
+    switch(json?.isTypeOf) {
+        case 'RateModelJump': return new RateModelJump(undefined, json)
+        case 'RateModelCurve': return new RateModelCurve(undefined, json)
+        default: throw new TypeError('Unknown json object passed as RateModel')
+    }
 }
