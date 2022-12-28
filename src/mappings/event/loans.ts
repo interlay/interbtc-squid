b6b2fdb65cff1f14aeae89dc0b8a35f7646ebaca
Matthias Funke <matthias.funke@alation.com>
2022-12-28 17:05:42 +0700

    Tested all loan events and found out about qToken

--- src/mappings/event/loans.ts
+++ src/mappings/event/loans.ts
@@ -19,7 +19,7 @@ import {
     LoansDepositedEvent,
     LoansDistributedBorrowerRewardEvent,
     LoansDistributedSupplierRewardEvent,
-    LoansNewMarketEvent, LoansRepaidBorrowEvent
+    LoansNewMarketEvent, LoansRedeemedEvent, LoansRepaidBorrowEvent, LoansWithdrawCollateralEvent
 } from "../../types/events";
 import { CurrencyId_Token as CurrencyId_Token_V6 } from "../../types/v6";
 import { CurrencyId_Token as CurrencyId_Token_V10 } from "../../types/v10";
@@ -35,7 +35,10 @@ import EntityBuffer from "../utils/entityBuffer";
 import { blockToHeight } from "../utils/heights";
 import { getCurrentRedeemPeriod } from "../utils/requestPeriods";
 import { getVaultId, getVaultIdLegacy } from "../_utils";
-import { LoanMarket } from "../../model/generated/loanMarket.model";
+import { LoanMarket} from "../../model/generated/loanMarket.model";
+import { Loan } from "../../model/generated/loan.model";
+import { Deposit } from "../../model/generated/deposit.model";
+import {Currency, currencySymbol} from "../../model/generated/_currency"
 
 export async function newMarket(
     ctx: Ctx,
@@ -71,17 +74,18 @@ export async function borrow(
     const currency = currencyId.encode(currencyOfLoan);
     const height = await blockToHeight(ctx, block.height, "LoansBorrowed");
     const account = address.interlay.encode(adress);
-    // await entityBuffer.pushEntity(
-    //     Transfer.name,
-    //     new Transfer({
-    //         id: item.event.id,
-    //         height,
-    //         timestamp: new Date(block.timestamp),
-    //         account: account,
-    //         token: currency,
-    //         amount,
-    //     })
-    console.log(`${adress} borrowed ${amount} ${currency}`);
+    await entityBuffer.pushEntity(
+        Loan.name,
+        new Loan({
+            id: item.event.id,
+            height: height,
+            timestamp: new Date(block.timestamp),
+            userParachainAddress: account,
+            token: currency,
+            amountBorrowed: amount,
+        })
+    );
+    console.log(`${account} borrowed ${amount} ${currencySymbol(currency)}`);
 }
 
 export async function depositCollateral(
@@ -91,6 +95,47 @@ export async function depositCollateral(
     entityBuffer: EntityBuffer
 ): Promise<void> {
     const rawEvent = new LoansDepositCollateralEvent(ctx, item.event);
+    const [adress, depositCurr, amount] = rawEvent.asV1020000;
+    const currency = currencyId.encode(depositCurr);
+    const height = await blockToHeight(ctx, block.height, "Deposit");
+    const account = address.interlay.encode(adress);
+    await entityBuffer.pushEntity(
+        Deposit.name,
+        new Deposit({
+            id: item.event.id,
+            height: height,
+            timestamp: new Date(block.timestamp),
+            userParachainAddress: account,
+            token: currency,
+            amountDeposited: amount,
+        })
+    );
+    console.log(`${account} deposited ${amount} ${currencySymbol(currency)} for collateral`);
+}
+
+export async function withdrawCollateral(
+    ctx: Ctx,
+    block: SubstrateBlock,
+    item: EventItem,
+    entityBuffer: EntityBuffer
+): Promise<void> {
+    const rawEvent = new LoansWithdrawCollateralEvent(ctx, item.event);
+    const [adress, depositCurr, amount] = rawEvent.asV1020000;
+    const currency = currencyId.encode(depositCurr);
+    const height = await blockToHeight(ctx, block.height, "WithdrawDeposit");
+    const account = address.interlay.encode(adress);
+    await entityBuffer.pushEntity(
+        Deposit.name,
+        new Deposit({
+            id: item.event.id,
+            height: height,
+            timestamp: new Date(block.timestamp),
+            userParachainAddress: account,
+            token: currency,
+            amountWithdrawn: amount,
+        })
+    );
+    console.log(`${account} withdrew ${amount} ${currencySymbol(currency)} from collateral`);
 }
 
 export async function depositForLending(
@@ -100,6 +145,22 @@ export async function depositForLending(
     entityBuffer: EntityBuffer
 ): Promise<void> {
     const rawEvent = new LoansDepositedEvent(ctx, item.event);
+    const [adress, depositCurr, amount] = rawEvent.asV1020000;
+    const currency = currencyId.encode(depositCurr);
+    const height = await blockToHeight(ctx, block.height, "Deposit");
+    const account = address.interlay.encode(adress);
+    await entityBuffer.pushEntity(
+        Deposit.name,
+        new Deposit({
+            id: item.event.id,
+            height: height,
+            timestamp: new Date(block.timestamp),
+            userParachainAddress: account,
+            token: currency,
+            amountDeposited: amount,
+        })
+    );
+    console.log(`${account} deposited ${amount} ${currencySymbol(currency)} for lending`);
 }
 
 export async function distributeBorrowerReward(
@@ -127,4 +188,46 @@ export async function repay(
     entityBuffer: EntityBuffer
 ): Promise<void> {
     const rawEvent = new LoansRepaidBorrowEvent(ctx, item.event);
+    const [adress, currencyOfLoan, amount] = rawEvent.asV1020000;
+    const currency = currencyId.encode(currencyOfLoan);
+    const height = await blockToHeight(ctx, block.height, "LoansRepaid");
+    const account = address.interlay.encode(adress);
+    await entityBuffer.pushEntity(
+        Loan.name,
+        new Loan({
+            id: item.event.id,
+            height: height,
+            timestamp: new Date(block.timestamp),
+            userParachainAddress: account,
+            token: currency,
+            amountRepaid: amount,
+        })
+    );
+    console.log(`${account} paid back ${amount} ${currencySymbol(currency)}`);
+}
+
+"Redeem means withdrawing a deposit by redeeming qTokens for Tokens."
+export async function withdrawDeposit(
+    ctx: Ctx,
+    block: SubstrateBlock,
+    item: EventItem,
+    entityBuffer: EntityBuffer
+): Promise<void> {
+    const rawEvent = new LoansRedeemedEvent(ctx, item.event);
+    const [adress, currencyOfLoan, amount] = rawEvent.asV1020000;
+    const currency = currencyId.encode(currencyOfLoan);
+    const height = await blockToHeight(ctx, block.height, "Redeemed");
+    const account = address.interlay.encode(adress);
+    await entityBuffer.pushEntity(
+        Deposit.name,
+        new Deposit({
+            id: item.event.id,
+            height: height,
+            timestamp: new Date(block.timestamp),
+            userParachainAddress: account,
+            token: currency,
+            amountWithdrawn: amount, // expand to 3 tokens: qToken, Token, equivalent in USD(T)
+        })
+    );
+    console.log(`${account} withdrew ${amount} ${currencySymbol(currency)} from deposit`);
 }
