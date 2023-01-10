833b211ec5f6d79eea7127a0ffa62a543338de33
jtfirek <106350168+jtfirek@users.noreply.github.com>
2023-01-10 10:32:14 -0500

    mistake fixed

--- src/mappings/event/redeem.ts
+++ src/mappings/event/redeem.ts
@@ -167,7 +167,7 @@ export async function executeRedeem(
 
     await entityBuffer.pushEntity(RedeemExecution.name, execution);
     await entityBuffer.pushEntity(Redeem.name, redeem);
-    const volumeTypes = [VolumeType.Issued, VolumeType.BridgeVolume];
+    const volumeTypes = [VolumeType.Redeemed, VolumeType.BridgeVolume];
     for (const volumeType of volumeTypes) {
         await entityBuffer.pushEntity(
             CumulativeVolume.name,
