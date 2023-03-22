827aa3286fef20e25ccccb052fe2e5fc01bf5370
Jacob Firek <jacob@interlay.io>
2023-03-22 12:37:09 -0400

    schema update

---
+++ src/model/generated/loanLiquidation.model.ts
@@ -0,0 +1,34 @@
+import {Entity as Entity_, Column as Column_, PrimaryColumn as PrimaryColumn_} from "typeorm"
+import * as marshal from "./marshal"
+import {Currency, fromJsonCurrency} from "./_currency"
+
+@Entity_()
+export class LoanLiquidation {
+    constructor(props?: Partial<LoanLiquidation>) {
+        Object.assign(this, props)
+    }
+
+    @PrimaryColumn_()
+    id!: string
+
+    @Column_("numeric", {transformer: marshal.bigintTransformer, nullable: false})
+    amountRepaid!: bigint
+
+    @Column_("jsonb", {transformer: {to: obj => obj.toJSON(), from: obj => obj == null ? undefined : fromJsonCurrency(obj)}, nullable: false})
+    amountRepaidToken!: Currency
+
+    @Column_("numeric", {transformer: marshal.bigintTransformer, nullable: false})
+    seizedCollateral!: bigint
+
+    @Column_("jsonb", {transformer: {to: obj => obj.toJSON(), from: obj => obj == null ? undefined : fromJsonCurrency(obj)}, nullable: false})
+    seizedCollateralToken!: Currency
+
+    @Column_("numeric", {transformer: marshal.bigintTransformer, nullable: false})
+    liquidationCost!: bigint
+
+    @Column_("jsonb", {transformer: {to: obj => obj.toJSON(), from: obj => obj == null ? undefined : fromJsonCurrency(obj)}, nullable: false})
+    liquidationCostToken!: Currency
+
+    @Column_("timestamp with time zone", {nullable: false})
+    timestamp!: Date
+}
